4677d3487a634f4e08892521f9a8a78937eb498f
Emil <emiljonsrud@gmail.com>
2023-03-13 19:24:24 +0100

    fixed errors

--- train_db.sql
+++ train_db.sql
@@ -1,46 +1,65 @@
 -- Track Section
-
 CREATE TABLE TrackSection(
   Name TEXT PRIMARY KEY NOT NULL,
+  ElectricTracks BOOL,
   StartStation TEXT NOT NULL, 
   EndStation TEXT NOT NULL, 
-  ElectricTracks BOOL,
-  FOREIGN KEY(StartStation) REFERENCES RaylwayStation(Name) ON UPDATE CASCADE ON DELETE CASCADE,
-  FOREIGN KEY(EndStation) REFERENCES RailwayStation(name) ON UPDATE CASCADE ON DELETE CASCADE);
-
+  FOREIGN KEY(StartStation) REFERENCES RailwayStation(Name) ON UPDATE CASCADE ON DELETE CASCADE,
+  FOREIGN KEY(EndStation) REFERENCES RailwayStation(name) ON UPDATE CASCADE ON DELETE CASCADE,
+  INSERT INTO TrackSubSection VALUES(StartStation, )
+);
 
  -- RailwayStation
-
-CREATE TABLE RailwayStation(Name TEXT PRIMARY KEY NOT NULL, 
-Height INT NOT NULL) 
+CREATE TABLE RailwayStation(
+  Name TEXT PRIMARY KEY NOT NULL, 
+  Height INT NOT NULL
+); 
 
 -- TrackSubSection
-CREATE TABLE TrackSubSection(SectionNo INT, 
-Length INT, 
-DoubleTrack BOOL,  
-StartsAt TEXT NOT NULL,
-EndsAt TEXT NOT NULL, 
-TrackSection TEXT NOT NULL, 
- FOREIGN KEY(StartsAt) REFERENCES Station(Name), FOREIGN KEY(EndsAT) REFERENCES Station(Name), 
-FOREIGN KEY(TrackSection) REFERENCES TrackSection(name),  CONSTRAINT PK_TrackSubSection PRIMARY KEY (SectionNO, TrackSection));
+CREATE TABLE TrackSubSection(
+  SectionNo INT, 
+  TrackSection TEXT NOT NULL, 
+  Length INT, 
+  DoubleTrack BOOL,  
+  StartsAt TEXT NOT NULL,
+  EndsAt TEXT NOT NULL, 
+  FOREIGN KEY(StartsAt) REFERENCES Station(Name), 
+  FOREIGN KEY(EndsAT) REFERENCES Station(Name), 
+  FOREIGN KEY(TrackSection) REFERENCES TrackSection(name),  
+  CONSTRAINT PK_TrackSubSection PRIMARY KEY (SectionNo, TrackSection)
+);
 
 --  TrainRoute    
 CREATE TABLE TrainRoute(
   Name TEXT PRIMARY KEY NOT NULL, 
   Operator TEXT, 
-  TrackSection TEXT NOT NULL, 
-  FOREIGN KEY(TrackSection) REFERENCES TrackSection(Name)
-)
+  TrackName TEXT NOT NULL, 
+  FOREIGN KEY(TrackName) REFERENCES TrackSection(Name)
+);
+
+-- WeekDay
+CREATE TABLE WeekDay(
+  Name TEXT PRIMARY KEY NOT NULL,
+);
+
+-- Day of Route
+CREATE TABLE DayOfRoute(
+  WeekDay VARCHAR(255),
+  TrainRoute VARCHAR(255),
+  FOREIGN KEY WeekName REFERENCES WeekDay(Name),
+  FOREIGN KEY TrainRoute REFERENCES TrainRoute(Name)
+  CONSTRAINT PK_DayOfRoute PRIMARY KEY(WeekName, TrainRoute)
+);
 
 -- Ticket
 CREATE TABLE Ticket(
-  TicketNo INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  TicketNo INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
   OrderID INT,
   CarID INT,
   PlaceNo INT,
   FOREIGN KEY(OrderID) REFERENCES CustomerOrder(OrderID),
-  FOREIGN KEY(CarID, PlaceNo) REFERENCES  Placement(CarID, PlaceNo),
-)
+  FOREIGN KEY(CarID, PlaceNo) REFERENCES  Placement(CarID, PlaceNo)
+);
 
 -- RouteStop
 CREATE TABLE RouteStop( 
@@ -48,14 +67,13 @@ CREATE TABLE RouteStop(
   TrainRoute TEXT, 
   FOREIGN KEY(RailwayStation) REFERENCES RailwayStation(name), 
   FOREIGN KEY(TrainRoute) REFERENCES TrainRoute(name), 
-  CONSTRAINT PK_RouteStop PRIMARY KEY(RailwayStation, 
-  TrainRoute
+  CONSTRAINT PK_RouteStop PRIMARY KEY(RailwayStation, TrainRoute)
 ); 
 
 
 -- TrainOccurance 
 CREATE TABLE TrainOccurance(
-  Date INT NOT NULL, 
+  Date DATE NOT NULL, 
   TrainRoute TEXT NOT NULL, 
   FOREIGN KEY(TrainRoute) REFERENCES TrainRoute(Name), 
   CONSTRAINT PK_TrainOccurance PRIMARY KEY(Date, TrainRoute) 
@@ -87,7 +105,7 @@ CREATE TABLE SleepCar(
 -- CustomerOrder
 CREATE TABLE CustomerOrder(
   OrderID INT PRIMARY KEY NOT NULL, 
-  OrderDate ???INT???, 
+  OrderDate DATE, 
   OrderTime INT, 
   CustomerID INT NOT NULL, 
   FOREIGN KEY(CustomerID) REFERENCES Customer(CustomerID)
