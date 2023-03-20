ae681cf70ad963345a0f83ea0c697fd49e3fcba5
Emil <emiljonsrud@gmail.com>
2023-03-20 23:17:09 +0100

    small tweaks on _user_input

--- Application.py
+++ Application.py
@@ -2,13 +2,13 @@
 from DB import DB
 
 WeekDay = {
-    "Monday"    : 1,
-    "Tuesday"   : 2,
-    "Wednesday" : 3,
-    "Thursday"  : 4,
-    "Friday"    : 5,
-    "Saturday"  : 6,
-    "Sunday"    : 7
+    1 : "Monday",
+    2 : "Tuesday",
+    3 : "Wednesday",
+    4 : "Thursday",
+    5 : "Friday",
+    6 : "Saturday",
+    7 : "Sunday",
 }
 
 class App:
@@ -19,28 +19,31 @@ class App:
         pass
 
     # --- Internal functions --- {{{
-    def __menu(self):
+    def _menu(self):
         """List possible functions and let user choose.""" 
         pass
-    def __user_response(self, alternatives: dict) -> int:
+
+    def _user_response(self, alternatives: dict) -> int:
         """Get a users respons given a list of alternatives.
         """
         msg = f"Please choose an alternative (0-{len(alternatives)})\n"
+        linebreak = "\n\n============================================\n"
         
         for key, val in alternatives.items():
-            msg += f"{val:>4}{key}\n"
+            msg += f"  ({str(key)+')':<4}{val}\n"
         msg += "0: exit to menu.\n"
         msg += "~> "
+
         while True:
             try:
                 response = int(input(msg))
             except ValueError:
-                print("Please insert an integer.")
-                continue
-            else:
+                print(linebreak + "Please insert an integer.\n")
+                continue                           
+            else:                                  
                 if response not in range(0, len(alternatives)):
-                    print("Pease choose a valid option")
-                    continue
+                    print(linebreak + "Pease choose a valid option\n")
+                    continue                       
                 else:
                     # Valid input, break the loop
                     break
@@ -50,9 +53,16 @@ class App:
     # --- }}}
 
     # --- User functions --- {{{
-    def get_train_routes(self):
+    def view_train_routes(self, db: DB):
         """Let user get information of trainroutes"""
+        response = self._user_response(WeekDay)
+        
         pass
     # --- }}}
+
+if __name__=="__main__":
+    app = App()
+    print(app._user_response(WeekDay))
+
 
 
