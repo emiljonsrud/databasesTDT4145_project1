f1342f94c6b6241fc0975e381c0b6851b885017c
Emil <emiljonsrud@gmail.com>
2023-03-20 22:21:45 +0100

    user response

--- Application.py
+++ Application.py
@@ -2,13 +2,13 @@
 from DB import DB
 
 WeekDay = {
-    "Monday"    : 0,
-    "Tuesday"   : 1,
-    "Wednesday" : 2,
-    "Thursday"  : 3,
-    "Friday"    : 4,
-    "Saturday"  : 5,
-    "Sunday"    : 6
+    "Monday"    : 1,
+    "Tuesday"   : 2,
+    "Wednesday" : 3,
+    "Thursday"  : 4,
+    "Friday"    : 5,
+    "Saturday"  : 6,
+    "Sunday"    : 7
 }
 
 class App:
@@ -25,7 +25,27 @@ class App:
     def __user_response(self, alternatives: dict) -> int:
         """Get a users respons given a list of alternatives.
         """
-        pass
+        msg = f"Please choose an alternative (0-{len(alternatives)})\n"
+        
+        for key, val in alternatives.items():
+            msg += f"{val:>4}{key}\n"
+        msg += "0: exit to menu.\n"
+        msg += "~> "
+        while True:
+            try:
+                response = int(input(msg))
+            except ValueError:
+                print("Please insert an integer.")
+                continue
+            else:
+                if response not in range(0, len(alternatives)):
+                    print("Pease choose a valid option")
+                    continue
+                else:
+                    # Valid input, break the loop
+                    break
+        return response
+        
 
     # --- }}}
 
