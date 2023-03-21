2c01336fd7fb0ccc545f955d320a282cde38da7a
Emil <emiljonsrud@gmail.com>
2023-03-21 12:06:13 +0100

    query for getting trainroutes on station

---
+++ queries/route_on_day.sql
@@ -0,0 +1,18 @@
+/* User arguments:
+  :weekday (WeekDay.Name)
+  :station (RailwayStation.Name)
+*/
+SELECT TR.Name 
+FROM RailwayStation AS RS 
+  INNER JOIN TrackSubSection AS TSS 
+    ON (RS.Name = TSS.StartsAt OR RS.Name = TSS.EndsAt)
+  INNER JOIN TrackSection AS TS 
+    ON (TSS.SectionName = TS.Name)
+  INNER JOIN TrainRoute AS TR 
+    ON (TS.Name = TR.SectionName)
+  INNER JOIN DayOfRoute AS DOR
+    ON (TR.Name = DOR.NameOfRoute)
+  INNER JOIN WeekDay AS WD
+    ON (DOR.NameOfDay = WD.Name)
+WHERE WD.Name = :weekday 
+  AND RS.Name = :station;
