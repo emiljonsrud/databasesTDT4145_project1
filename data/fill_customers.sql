9757193c73a08665d8a06d6720341ee92d4b39ad
Emil <emiljonsrud@gmail.com>
2023-03-21 23:58:59 +0100

    created user dummydata

---
+++ data/fill_customers.sql
@@ -0,0 +1,9 @@
+/* 
+  Fill dummy customer data
+*/
+
+INSERT INTO Customer (Name, PhoneNO, Email)
+VALUES
+  ("Ola Nordman", 47474747, "ola@norge.no"),
+  ("Rolf Rolfsen", 9009100, "rullerolf@online.com"),
+  ("Hermine Grange", 10020343, "hermgra@stud.galtvort.no");
