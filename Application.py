01f371d25ae9816081ab6a1cafa83b60e0e5d982
Emil <emiljonsrud@gmail.com>
2023-03-23 17:35:41 +0100

    format car now works for all types

--- Application.py
+++ Application.py
@@ -150,23 +150,45 @@ class App:
         """Formats the rows of an returned sql-query into a nice table."""
         return tabulate(rows, **kwargs)    
         
-    def _format_chair_car(self, n_rows: int, seats_per_row: int, reserved_seats: list[int]) -> str:
+    def _format_car(self, n_compartments: int, n_rows: int, seats_per_row: int, reserved_seats: list[int]) -> str:
         """Formats information about a car into a nice table.
         -- Inputs
-        :param n_rows: number of rows in the carrige
-        :param seats_per_row: number of seats per row in the car
+        :param n_compartments: number of compartments in the carrige (can be 0)
+        :param n_rows: number of rows in the carrige (can be 0)
+        :param seats_per_row: number of seats per row in the car (can be 0)
         :parram reserved_seats: list of seats that are reserved (1-indexed)
         """
+        # Assert paramaters depending on car-type
+        if n_compartments:
+            seats_per_row = 2
+            n_rows = n_compartments
+            mid_seat = 0
+        else:
+            mid_seat = int(np.ceil(seats_per_row/2))
         n_seats = n_rows * seats_per_row
+
+        # Verify validity of reserved_seats
         if n_seats < max(reserved_seats):
             raise ValueError(f"Only {n_seats} available. Can not reserve seat {max(reserved_seats)}")
 
+        # Mark reseved seats
         seats = np.arange(1, n_seats+1).tolist()
         for i in reserved_seats:
             seats[i-1] = "🚫"
-        seats = np.array([str(seats[i]) + "   " + str(seats[i+1]) for i in range(0, n_seats, 2)]).reshape(n_rows, int(seats_per_row/2))
 
-        return self._format_rows(seats, tablefmt="rounded_grid")
+
+        # Divide into two columns
+        formatted_seats = [[" " for _ in range(2)] for _ in range(n_rows)]
+        for i in range(n_rows):
+            curr_seat = i*seats_per_row
+
+            left_col = [f"{seats[j]:>3}" for j in range(curr_seat, curr_seat+mid_seat)]
+            right_col = [f"{seats[j]:>3}" for j in range(curr_seat+mid_seat, curr_seat+seats_per_row)]
+
+            formatted_seats[i][0] = "".join(left_col)
+            formatted_seats[i][1] = "".join(right_col)
+
+        return self._format_rows(formatted_seats, tablefmt="rounded_grid", colalign=("center","center"))
         
     # --- }}}
 
@@ -257,14 +279,22 @@ class App:
         # TODO create get available tickets query
 
         # Temporary:
+        chair_car = True
         tickets = [1, 6, 8]
-        n_rows = 3
-        seats_per_row = 4
+        n_rows = 0
+        seats_per_row = 0
+        n_compartments = 4
 
-        car_overview = self._format_chair_car(n_rows, seats_per_row, tickets)
-        options = np.delete(np.arange(1, n_rows*seats_per_row+1), np.array(tickets)-1).tolist()
+        car_overview = self._format_car(n_compartments, n_rows, seats_per_row, tickets)
 
-        self._user_option_response(msg=car_overview, options=options, multi_select=True, show_multi_select_hint=True)
+        # options = np.delete(np.arange(1, n_rows*seats_per_row+1), np.array(tickets)-1).tolist()
+        options = np.delete(np.arange(1, (2*n_compartments)+1), np.array(tickets)-1).tolist()
+
+        try:
+            user_booking = self._user_option_response(msg=car_overview, options=options, multi_select=True, show_multi_select_hint=True)
+        except SystemExit:
+            return
+
 
 
 
@@ -278,6 +308,12 @@ class App:
         pass
 
 
+
+    # --- }}}
+    # --- View customer orders --- {{{
+    def view_customer_orders(self):
+        """View custmer orders"""
+        pass
 
     # --- }}}
         
