56e5c9bd436539e30144f9eb7855cc6a965739de
karenpernille <karenpernille@icloud.com>
2023-03-23 21:29:36 +0100

    working (not perfect yet) :) finding taken seats

--- queries/get_taken_seats.sql
+++ queries/get_taken_seats.sql
@@ -3,6 +3,14 @@ Input: run_date, name_of_route, car_id,
        start_station, end_station
 */
 
-
+SELECT Placement.PlaceNo
+FROM
+    Ticket INNER JOIN TicketOnSection
+        ON (Ticket.TicketNo = TicketOnSection.TicketNo)
+    INNER JOIN Placement 
+        ON (Ticket.PlaceNo = Placement.PlaceNO)
+WHERE Placement.CarID = :car_id AND TicketOnSection.StartStation = :start_station
+    AND TicketOnSection.EndStation = :end_station AND Ticket.RunDate = :run_date
+    AND Ticket.NameOfRoute = :name_of_route;
 
 
