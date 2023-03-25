99fa702eee667b4b69acd97792a02960a660d497
Emil <emiljonsrud@gmail.com>
2023-03-25 21:22:13 +0100

    query takes account for routes going reverse direction

--- queries/routes_between_stations.sql
+++ queries/routes_between_stations.sql
@@ -1,13 +1,39 @@
 /*
 User input:
-    date_
-    start_station
-    end_station
-    time_
+    :date_
+    :start_station
+    :end_station
+    :time_
 */
 
 
+SELECT DISTINCT TR.Name, TOc.RunDate, RS1.Station, RS1.TimeOfDay, RS2.Station, RS2.TimeOfDay
+FROM TrainRoute AS TR
+    INNER JOIN TrainOccurance AS TOc
+        ON TR.Name = TOc.NameOfRoute
+    INNER JOIN TrackSection AS TS
+        ON TR.SectionName = TS.Name
+    INNER JOIN RouteStop AS RS1
+        ON TR.Name = RS1.NameOfRoute
+    INNER JOIN RouteStop AS RS2
+        ON TR.Name = RS2.NameOfRoute
+    LEFT OUTER JOIN TrackSubSection AS TSS1
+        ON RS1.Station = TSS1.StartsAt
+    LEFT OUTER JOIN TrackSubSection AS TSS2
+        ON RS1.Station = TSS2.StartsAt
+WHERE 
+    (
+        (TR.SectionMainDirection = 1 AND TSS1.SubSectionNo > TSS2.SubSectionNo)
+        OR (TR.SectionMainDirection = 0 AND TSS1.SubSectionNo > TSS2.SubSectionNo)
+        OR (TR.SectionMainDirection = 0 AND TSS1.SubSectionNo IS NULL)
+    )
+    AND (RS1.Station = :start_station AND RS2.Station = :end_station)
+    AND TOc.RunDate >= :date_
+    AND RS1.TimeOfDay >= :time_
 
+
+
+/*
 SELECT DISTINCT TR.Name, RS.TimeOfDay, TOCC.RunDate ---TODO   after tidspunkt !!!!!!!
 FROM 
     ((TrainOccurance AS TOCC INNER JOIN TrainRoute AS TR 
@@ -45,7 +71,7 @@ FROM
     AND  TSS1.SubSectionNo <= TSS2.SubSectionNo
     ORDER BY TOCC.Rundate ASC, RS.TimeOfDay ASC;
 
-
+*/
 
 
 /*
